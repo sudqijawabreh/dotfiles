e7edaf047ac0f4ac10ae9ab796f90b222b0815a6
Sudqi Jawabreh <sudqiomar@gmail.com>
2024-12-02 22:50:36 +0200

    Fix mute icon

--- scripts/MuteVolume.sh
+++ scripts/MuteVolume.sh
@@ -7,9 +7,20 @@ amixer set Master toggle > /dev/null
 # Get the current mute status
 status=$(amixer get Master | grep '\[on\]')
 
+value="$(amixer get Master | grep -o '[0-9]*%' | head -1 | tr -d '%')"
+icon=audio-volume-high
+
+if [ "$value" -lt 30 ]; then
+    icon=audio-volume-low
+elif [ "$value" -lt 80 ]; then
+    icon=audio-volume-medium
+else
+    icon=audio-volume-high
+fi
+
 if [[ -n $status ]]; then
     # Sound is on
-    notify-send -a "volume-notifier" "Sound" "Unmuted" -i audio-volume-high -h int:value:"$(amixer get Master | grep -o '[0-9]*%' | head -1 | tr -d '%')"
+    notify-send -a "volume-notifier" "Sound" "Unmuted" -i $icon -h int:value:"$(amixer get Master | grep -o '[0-9]*%' | head -1 | tr -d '%')"
 else
     # Sound is off
     notify-send -a "volume-notifier" "Sound" "Muted" -i audio-volume-muted -h int:value:"$(amixer get Master | grep -o '[0-9]*%' | head -1 | tr -d '%')"
